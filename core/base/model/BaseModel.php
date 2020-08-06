ff75f833ab845d6fdcbecf5875356dc1a7ad6763
Edward <edik180496@gmail.com>
2020-08-06 18:59:26 +1000

    connect MYSQL, test query

---
+++ core/base/model/BaseModel.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace core\base\model;
+
+
+use core\base\controller\Singleton;
+use core\base\exceptions\DbException;
+
+class BaseModel
+{
+
+    use Singleton;
+
+    protected $db;
+
+    private function __construct()
+    {
+        $this->db = @new \mysqli(HOST, USER, PASS, DB_NAME);
+
+        if($this->db->connect_error){
+
+            throw new DbException('Ошибка подключения к базе данных: '
+                . $this->db->connect_errno . ' ' . $this->db->connect_error);
+        }
+
+        $this->db->query("SET NAMES UTF8");
+    }
+
+}
