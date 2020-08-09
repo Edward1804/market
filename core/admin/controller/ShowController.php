e2fcf789cdb6b0c8a231fd59094ca9b4a8cf823b
Edward <edik180496@gmail.com>
2020-08-09 22:58:08 +1000

    fix bugs

--- core/admin/controller/ShowController.php
+++ core/admin/controller/ShowController.php
@@ -4,6 +4,8 @@
 namespace core\admin\controller;
 
 
+use core\base\settings\Settings;
+
 class ShowController extends BaseAdmin
 {
 
@@ -13,7 +15,7 @@ class ShowController extends BaseAdmin
 
         $this->createTableData();
 
-        $this->createData();
+        $this->createData(['fields' => 'content']);
 
         return $this->expansion(get_defined_vars());
 
@@ -21,6 +23,81 @@ class ShowController extends BaseAdmin
 
     protected function outputData(){
 
+
+    }
+
+    protected function createData($arr = []){
+
+        $fields = [];
+        $order = [];
+        $order_directions = [];
+
+
+
+        if(!$this->columns['id_row']) return $this->data = [];
+
+        $fields[] = $this->columns['id_row'] . ' as id';
+        if($this->columns['name']) $fields['name'] = 'name';
+        if($this->columns['img']) $fields['img'] = 'img';
+
+        if(count($fields) < 3){
+            foreach ($this->columns as $key => $item){
+                if(!$fields['name'] && strpos($key, 'name') !== false){
+                    $fields['name'] = $key . ' as name';
+                }
+                if(!$fields['img'] && strpos($key, 'img') === 0){
+                    $fields['img'] = $key . ' as img';
+                }
+
+            }
+        }
+
+        if($arr['fields']){
+            if(is_array($arr['fields'])){
+                $fields = Settings::instance()->arrayMergeRecursive($fields, $arr['fields']);
+            }else{
+                $fields[] = $arr['fields'];
+            }
+
+        }
+
+        if($this->columns['parent_id']){
+            if(!in_array('parent_id', $fields)) $fields[] = 'parent_id';
+            $order[] = 'parent_id';
+        }
+
+        if($this->columns['menu_position']) $order[] = 'menu_position';
+        elseif ($this->columns['date']){
+
+            if($order) $order_directions = ['ASC', 'DESC'];
+            else $order_directions[] = 'DESC';
+
+            $order[] = 'date';
+
+        }
+
+        if($arr['order']){
+            if(is_array($arr['order'])){
+                $order = Settings::instance()->arrayMergeRecursive($order, $arr['order']);
+            }else{
+                $order[ ] = $arr['order'];
+            }
+
+        }
+        if($arr['order_directions']){
+            if(is_array($arr['order_directions'])) {
+                $order_directions = Settings::instance()->arrayMergeRecursive($order_directions, $arr['order_directions']);
+            }else{
+                $order_directions[] = $arr['order_directions'];
+            }
+        }
+
+
+        $this->data = $this->model->get($this->table, [
+            'fields' => $fields,
+            'order' => $order,
+            'order_directions' => $order_directions
+        ]);
 
     }
 
--- core/admin/model/Model.php
+++ core/admin/model/Model.php
@@ -5,10 +5,13 @@ namespace core\admin\model;
 
 
 
+use core\base\controller\Singleton;
 use core\base\model\BaseModel;
 
 class Model extends BaseModel
 {
+
+    use Singleton;
 
 
 
